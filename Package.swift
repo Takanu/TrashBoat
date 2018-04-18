a00f2f956e4ae03d2cc5cf8c69370ca6635e59fc
Takanu Kyriako <takanu@users.noreply.github.com>
2018-04-18 23:47:09 +0100

    Changed the Pelican dependency to a fixed revision.

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
 		.library(name: "TrashBoat", targets: ["TrashBoat"]),
 	],
 	dependencies: [
-		.package(url: "https://github.com/Takanu/Pelican.git", .branch("master")),
+		.package(url: "https://github.com/Takanu/Pelican.git", .revision("9b3c3111b243c26242b91972ca17081f6ae1229c")),
 	],
 	targets: [
 		.target(name: "TrashBoat", dependencies: ["Pelican"]),
